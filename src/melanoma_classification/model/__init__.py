40fe1d949622647c091582e2e9117210c25ad57d
„Irish-77“ <bastian.berle@gmail.com>
2024-10-20 18:09:34 +0200

    Add evaluation of training and model

--- src/melanoma_classification/model/__init__.py
+++ src/melanoma_classification/model/__init__.py
@@ -1,6 +1,8 @@
 from melanoma_classification.model.instances import get_dermmel_classifier_v1
+from melanoma_classification.model.vision_transformer import VisionTransformer
 
 
 __all__ = [
-    "get_dermmel_classifier_v1"
+    "get_dermmel_classifier_v1",
+    "VisionTransformer"
 ]
